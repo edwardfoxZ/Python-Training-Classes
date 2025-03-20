22d12cb73dba5332fe7d769c4260feb7605f912c
CodeEdward <108403615+edwardfox15@users.noreply.github.com>
2025-03-20 16:58:03 +0000

    Add Flatmates_Bill

---
+++ Flatmates_Bill/main.py
@@ -0,0 +1,14 @@
+
+
+class Flatmate_Bill:
+
+    def __init__(self, id, payment):
+        self.id = id
+        self.payment = payment
+
+# class App:
+#     def __init__(self, shares):
+
+
+flBill = Flatmate_Bill(1, 300)
+print(flBill)
